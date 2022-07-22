b695be184503aa306bed388cfaeb1f7b199e1916
Hannah Steil <h-steil@t-online.de>
2022-07-22 12:59:15 +0200

    Add onclick function

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,15 @@ import "./Card.css";
 export default function Card({ colorCode }) {
   return (
     <div className="card" style={{ backgroundColor: colorCode }}>
-      <button type="button"> {colorCode} </button>
+      <button
+        type="button"
+        onClick={() => {
+          navigator.clipboard.writeText(colorCode);
+        }}
+      >
+        {" "}
+        {colorCode}{" "}
+      </button>
     </div>
   );
 }
