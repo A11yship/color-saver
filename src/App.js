d4146f101e0f7873f5912f6b4cc229b7b6bc221a
Hannah Steil <h-steil@t-online.de>
2022-07-22 11:41:42 +0200

    Add dataset

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
 import "./App.css";
+import { nanoid } from "nanoid";
+
+const colors = [
+  { id: nanoid(), colorCode: "0d3b66" },
+  { id: nanoid(), colorCode: "faf0ca" },
+  { id: nanoid(), colorCode: "f4d35e" },
+  { id: nanoid(), colorCode: "EE964B" },
+  { id: nanoid(), colorCode: "F95738" },
+  { id: nanoid(), colorCode: "4EFFEF" },
+  { id: nanoid(), colorCode: "73A6AD" },
+  { id: nanoid(), colorCode: "9B97B2" },
+  { id: nanoid(), colorCode: "D8A7CA" },
+  { id: nanoid(), colorCode: "C7B8EA" },
+  { id: nanoid(), colorCode: "7899D4" },
+];
 
 function App() {
   return (
