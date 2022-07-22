4966243d6cb0a7e10ca02cdc9b7f804979f781af
Hannah Steil <h-steil@t-online.de>
2022-07-22 11:30:14 +0200

    Add prototype for showing colorcards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
 
 function App() {
-  return <div>TestTestTest</div>;
+  return (
+    <>
+      <h1> Color-Saver </h1>
+      <div className="color_cards">
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+        <div className="color_card">
+          <button type="button"> name </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
