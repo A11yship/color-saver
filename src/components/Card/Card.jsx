46ba0d5e707e0da675565a18acb92eb2e21f584f
Hannah Steil <h-steil@t-online.de>
2022-07-25 00:21:22 +0200

    Style delete button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,10 @@ import "./Card.css";
 export default function Card({ colorCode }) {
   return (
     <div className="card" style={{ backgroundColor: colorCode }}>
+      <button type="button" className="delete">
+        {" "}
+        ⓧ{" "}
+      </button>
       <button
         type="button"
         onClick={() => {
